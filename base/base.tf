615bb4172af8c387e5f43f97f158ea752b8cd613
matsu-chara <matsuy00@gmail.com>
2018-06-03 14:26:06 +0900

    add sg

--- base/base.tf
+++ base/base.tf
@@ -12,3 +12,24 @@ resource "aws_eip" "base" {
   instance = "${aws_instance.base.id}"
   vpc = true
 }
+
+resource "aws_security_group" "allow_http_ssh" {
+  name = "allow_http_ssh"
+  description = "allow http/ssh"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags {
+    Name = "web-server"
+    Made = "terraform"
+  }
+}
