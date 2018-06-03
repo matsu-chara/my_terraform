b1909346c5cde2a6d051d7cd856ac0fbdd92db70
matsu-chara <matsuy00@gmail.com>
2018-06-03 23:18:32 +0900

    remove unused var

--- base/variables.tf
+++ base/variables.tf
@@ -12,9 +12,3 @@ variable "ami" {
     "us-west-1"      = "ami07c4b331c"
   }
 }
-
-variable "security_group_ids" {
-  type        = "list"
-  description = "List of security group IDs"
-  default     = ["sg-9742f9ef", "sg-9742f9ef", "sg-9742f9ef"]
-}
