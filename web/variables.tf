378145aedb04e43c4abf71e64b3748089f0b474c
matsu-chara <matsuy00@gmail.com>
2018-06-03 18:24:45 +0900

    more sg and fmt

--- web/variables.tf
+++ web/variables.tf
@@ -11,15 +11,16 @@ variable "public_key_path" {
 }
 
 variable "ami" {
-  type = "map"
+  type        = "map"
   description = "A map of AMIs"
+
   default = {
     "ap-northeast-1" = "ami-2724cf58"
-    "us-west-1" = "ami07c4b331c"
+    "us-west-1"      = "ami07c4b331c"
   }
 }
 
 variable "instance_type" {
   description = "instance type."
-  default = "t2.micro"
+  default     = "t2.micro"
 }
--- web/web.tf
+++ web/web.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "./vpc"
-  name = "web"
-  cidr = "10.0.0.0/16"
+  source        = "./vpc"
+  name          = "web"
+  cidr          = "10.0.0.0/16"
   public_subnet = "10.0.1.0/24"
 }
 
@@ -15,31 +15,94 @@ resource "aws_key_pair" "auth" {
 }
 
 resource "aws_instance" "web" {
-  ami = "${lookup(var.ami, var.region)}}"
-  instance_type = "${var.instance_type}"
-  key_name = "${var.key_name}}"
-  subnet_id = "${module.vpc.public_subnet_id}"
+  ami                         = "${lookup(var.ami, var.region)}}"
+  instance_type               = "${var.instance_type}"
+  key_name                    = "${var.key_name}}"
+  subnet_id                   = "${module.vpc.public_subnet_id}"
   associate_public_ip_address = true
-  user_data = "${file("files/web_bootstrap.sh")}}"
+  user_data                   = "${file("files/web_bootstrap.sh")}}"
 
   vpc_security_group_ids = [
-    "${aws_security_group.web_host_sg.id}"
+    "${aws_security_group.web_host_sg.id}",
   ]
+
   count = 2
 }
 
 resource "aws_elb" "web" {
-  name = "web-elb"
-  subnets = ["${module.vpc.public_subnet_id}"]
+  name            = "web-elb"
+  subnets         = ["${module.vpc.public_subnet_id}"]
   security_groups = ["${aws_security_group.web_inbound_sg.id}"]
+
   "listener" {
-    instance_port = 80
-    instance_protocol = ""
-    lb_port = 0
-    lb_protocol = ""
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
   }
+
   instances = ["${aws_instance.web.*.id}"]
 }
 
-resource "aws_security_group" "web_inbound_sg" {[
-]}
+resource "aws_security_group" "web_inbound_sg" {
+  name        = "web_inbound"
+  description = "Allow HTTP from Anywhere"
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8
+    protocol    = "icmp"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "web_host_sg" {
+  name        = "web_host"
+  description = "Allow SSH * HTTP to web hosts"
+  vpc_id      = "${module.vpc.vpc_id}}"
+
+  ingress {
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8
+    protocol    = "icmp"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  # HTTP access from the VPC
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+    cidr_blocks = ["${module.vpc.cidr}"]
+  }
+
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
