0753feda46a9db6dba546ebde68b3ccf3d3874eb
matsu-chara <matsuy00@gmail.com>
2018-06-03 12:43:45 +0900

    add eip and fmt

--- base/base.tf
+++ base/base.tf
@@ -1,9 +1,13 @@
 provider "aws" {
-  region = "ap-northeast-1"
+  region  = "ap-northeast-1"
   version = "~> 1.21"
 }
 
 resource "aws_instance" "base" {
-  ami = "ami-2724cf58"
+  ami           = "ami-2724cf58"
   instance_type = "t2.micro"
+}
+
+resource "aws_eip" "base" {
+  instance = "${aws_instance.base.id}"
 }
