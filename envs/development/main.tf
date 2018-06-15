df389db4751d91170298e0bef31bd8e551ac26cf
matsu-chara <matsuy00@gmail.com>
2018-06-15 22:59:04 +0900

    add main

--- envs/development/main.tf
+++ envs/development/main.tf
@@ -12,5 +12,8 @@ module "vpc" {
   source = "github.com/turnbullpress/tf_vpc.git?ref=v0.0.2"
   environment = "${var.environment}"
   region = "${var.region}"
+  key_name = "${var.key_name}"
+  vpc_cidr = "${var.vpc_cidr}"
+  public_subnets = ["${var.public_subnets}"]
+  private_subnets = ["${var.private_subnets}"]
 }
-
