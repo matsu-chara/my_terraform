43ffb36216fe49fd1698af578b5834704690d659
matsu-chara <matsuy00@gmail.com>
2018-06-16 00:06:46 +0900

    add prod

---
+++ envs/production/main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+terraform {
+  backend "s3" {
+    region = "ap-northeast-1"
+    bucket = "examplecom-remote-state-development"
+    key = "terraform.tfstate"
+  }
+}
+
+module "remote_state" {
+  source = "github.com/turnbullpress/tf_remote_state.git"
+  prefix = "${var.prefix}"
+  environment = "${var.environment}"
+}
+
+module "vpc" {
+  source = "github.com/turnbullpress/tf_vpc.git?ref=v0.0.2"
+  environment = "${var.environment}"
+  region = "${var.region}"
+  key_name = "${var.key_name}"
+  vpc_cidr = "${var.vpc_cidr}"
+  public_subnets = ["${var.public_subnets}"]
+  private_subnets = ["${var.private_subnets}"]
+}
+
+output "public_subnet_ids" {
+  value = ["${module.vpc.public_subnet_ids}"]
+}
+
+output "privatesubnet_ids" {
+  value = ["${module.vpc.private_subnet_ids}"]
+}
+
+output "bastion_host_dns" {
+  value = "${module.vpc.bastion_host_dns}"
+}
+
+output "bastion_host_ip" {
+    value = "${module.vpc.bastion_host_ip}"
+}
