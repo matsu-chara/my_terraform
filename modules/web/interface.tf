86c02a6283854e8d4298f32d62f423fc9be788e4
matsu-chara <matsuy00@gmail.com>
2018-06-16 00:00:48 +0900

    add web

--- modules/web/interface.tf
+++ modules/web/interface.tf
@@ -25,3 +25,13 @@ variable "domain" {}
 variable "web_instance_count" {}
 variable "app_instance_count" {}
 
+output "web_elb_address" {
+  value = "${ aws_elb.web.dns_name}"
+}
+output "web_host_addresses" {
+  value = "${ aws_instance.web.*. private_ip}"
+}
+output "app_host_addresses" {
+  value = "${ aws_instance.app.*. private_ip}"
+}
+
